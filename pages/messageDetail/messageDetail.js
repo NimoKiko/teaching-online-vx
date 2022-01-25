f35cea2ddaee1b3f6ba1a92db2e834156a76a278
黄逸凡 <hyf@bearhunting.cn>
2022-01-25 13:38:00 +0800

    新增消息详情页面

---
+++ pages/messageDetail/messageDetail.js
@@ -0,0 +1,67 @@
+// pages/messageDetail/messageDetail.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    type:1,
+    content:"成为iu复合物i恶化覅u文化覅u我微博覅u文化覅u乌黑u发和覅u乌黑u凤凰网二v回味u回复货物i俄方我和覅u乌黑i返回维护v分为氛围捏v叫我i恶杰佛为我iu恶化v南无阿回复我v你和我二v你好为uv和我i俄红粉v为红v你还问u发我欸服务呢"
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
